56c9ee5feedf6fa98efa68965c809c3ec37550d2
Parth Dhengle <parthdhengle12@gmail.com>
2026-02-26 00:10:12 +0530

    Time: 3 ms (1.46%) | Memory: 34.4 MB (8.91%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* helper(TreeNode* node){
+        if(node->left==NULL){
+            return node->right;
+        }else if(node->right==NULL){
+            return node->left;
+        }
+        TreeNode* rightchild=node->right;
+        TreeNode* rightmost=node->left;
+        while(rightmost->right!=NULL){
+            rightmost=rightmost->right;
+        }
+        rightmost->right=rightchild;
+        return node->left;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val==key){return helper(root);}
+        TreeNode* demo=root;
+        while(root!=NULL){
+            if(root->val>key){
+                if(root->left and root->left->val==key){
+                    root->left=helper(root->left);
+                    break;
+                }
+                else{root=root->left;}
+            }else{
+                if(root->right and root->right->val==key){
+                    root->right=helper(root->right);
+                    break;
+                }
+                else{root=root->right;}
+            }
+        }
+        return demo;
+    }
+};
