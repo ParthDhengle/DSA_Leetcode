346fece6587d9a0bab0d76b96e39e3a2258324dd
Parth Dhengle <parthdhengle12@gmail.com>
2026-04-04 15:56:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (67.76%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-int recursion(int m,int n,int i,int j, vector<vector<int>>&dp){
-    if(i==n-1 and j==m-1){
-        return 1;
-    }if(i >=n or j>=m){return 0;}
-    if (dp[i][j]!=-1){return dp[i][j];}
-    return dp[i][j]=recursion(m,n,i+1,j,dp)+recursion(m,n,i,j+1,dp);
-}
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return recursion(m,n,0,0,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,1));
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
