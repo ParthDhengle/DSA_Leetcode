38d81f49a0ac1d71e4b49b45127aaf1f3225881b
Parth Dhengle <parthdhengle12@gmail.com>
2026-05-14 17:34:20 +0530

    Time: 3 ms (28.23%) | Memory: 28.4 MB (7.60%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>hash;
+        for(int i=0;i<n;i++){
+            hash[nums[i]]++;
+        }
+        for(auto i : hash){
+            if(i.second > n/2){
+                return i.first;
+            }
+        }
+        return 0;
+    }
+};
