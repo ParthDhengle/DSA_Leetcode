594dd4b189a542fd92eee39ea9c44513ac3f9592
Parth Dhengle <parthdhengle12@gmail.com>
2026-05-28 22:47:08 +0530

    Time: 131 ms (5.06%) | Memory: 8.9 MB (99.48%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -5,23 +5,12 @@ public:
         int m=needle.size();
         int k=0;
         for(int i=0;i<n;i++){
-            // cout<<"i : "<<i<<" k: "<<k<<endl;
-            if(haystack[i]==needle[0]){
-                int j=i;
-                k=0;
-                
-                while(j<n and k<m){
-                    
-                    if(haystack[j]==needle[k]){
-                        
-                        j++;
-                        k++;
-                    }else{
-                        break;
-                    }
-                }
-                if(k==m){return i;}
+            k=0; 
+            while(k<m and haystack[i+k]==needle[k]){
+                k++;
             }
+            if(k==m){return i;}
+            
         }
         return -1;
     }
