06e0f8a7653132c1cfbb3b472915e19e22abf5ed
Parth Dhengle <parthdhengle12@gmail.com>
2026-06-12 15:46:20 +0530

    Time: 498 ms (54.82%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -3,5 +3,10 @@
 -- select w2.id from weather w1
 -- where w1.temperature < (select w2.temperature from weather w2 where w2.recordDate=date_add(w1.recordDate, interval 1 day));
 
-select w1.id from weather w1
-where w1.temperature > (select w2.temperature from weather w2 where w2.recordDate = date_add(w1.recordDate, interval -1 day));
+-- select w1.id from weather w1
+-- where w1.temperature > (select w2.temperature from weather w2 where w2.recordDate = date_add(w1.recordDate, interval -1 day));
+
+select today.id from weather today
+join weather yesterday 
+    on datediff(today.recordDate, yesterday.recordDate)=1
+where today.temperature > yesterday.temperature;
