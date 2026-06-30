5842877aadd58cb0a4de5c95f19dfecffb6b9d24
Parth Dhengle <parthdhengle12@gmail.com>
2026-06-30 18:48:24 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (58.84%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,36 +11,25 @@
  */
 class Solution {
 public:
-    TreeNode* create(TreeNode * node, vector<int>& inorder, vector<int>& postorder){
-        int in=inorder.size();
-        int po=postorder.size();
-        int idx=0;
-        for(int i=0;i<in;i++){
-            if(inorder[i]==node->val){
-                idx=i;
-                break;
-            }
-        }
-        vector<int>left_inorder(inorder.begin(),inorder.begin()+idx);
-        int left_inorder_size=left_inorder.size();
-        vector<int>right_inorder(inorder.begin()+idx+1,inorder.end());
-        vector<int>left_postorder(postorder.begin(), postorder.begin()+left_inorder_size);
-        vector<int>right_postorder(postorder.begin()+left_inorder_size,postorder.end()-1);
+TreeNode* tree(vector<int>& inorder, vector<int>& postorder, int& index,unordered_map<int,int>&mp , int start,int end ){
+    if(start>end or index<0)return NULL;
+    TreeNode* tn=new TreeNode(postorder[index]);
 
-        if(!left_postorder.empty()){
-            node->left=create(new TreeNode(left_postorder.back()) , left_inorder, left_postorder);
-        }else{
-            node->left=NULL;
-        }
-        if(!right_postorder.empty()){
-            node->right=create(new TreeNode(right_postorder.back()) , right_inorder , right_postorder) ; 
-        }else{
-            node->right=NULL;
-        }
-        return node;
+    int inorder_idx=mp[postorder[index]];
+    index--;
 
-    }
+    tn->right=tree(inorder, postorder,index,mp, inorder_idx+1,end);
+    tn->left=tree(inorder,postorder, index, mp, start,inorder_idx-1);
+
+    return tn;
+}
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        return create(new TreeNode(postorder.back()) , inorder,postorder);
+        unordered_map<int,int>mp;
+        int n=inorder.size();
+        for(int i=0;i<n;i++){
+            mp[inorder[i]] =  i;
+        }
+        int index=n-1;
+        return tree(inorder,postorder,index,mp,0,n-1);
     }
 };
