1765652148574fd6ab6e1c5ea2980efcc5540314
Parth Dhengle <parthdhengle12@gmail.com>
2026-07-12 20:14:04 +0530

    Time: 6 ms (49.95%) | Memory: 20 MB (38.41%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-    bool rec(int i, unordered_map<int,vector<int>>&mp , unordered_set<int> visited , unordered_set<int>&mem){
-        if(visited.find(i)!=visited.end()){return false;}
-        if(mem.find(i)!=mem.end())return true;
-        visited.insert(i);
-        if(mp.find(i) != mp.end()){
-            for(int nei : mp[i]) {
-                if(!rec(nei, mp, visited, mem)) return false;
-            }
+    bool dfs(int i, vector<vector<int>>&adj , vector<int>& state){
+        if(state[i]==1){return false;}
+        if(state[i]==2){return true;}
+        state[i]=1;
+
+        for(int nei : adj[i]) {
+            if(!dfs(nei, adj,state)) return false;
         }
-        mem.insert(i);
+        state[i]=2;
         return true;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,vector<int>>mp;
-        int n=prerequisites.size();
+        vector<vector<int>>adj(numCourses);
+
         for(auto i :prerequisites ){
-            mp[i[0]].push_back(i[1]);
+            adj[i[0]].push_back(i[1]);
         }
-        //if reachable add to mem; 
-        unordered_set<int>mem;
+        vector<int>state(numCourses,0);
 
-        for(int i=0;i<n;i++){
-            int curr=prerequisites[i][0];
-
-            unordered_set<int>visited;
-            if(mem.find(curr)==mem.end() and !rec(curr, mp, visited, mem))return false;
-            mem.insert(curr);
+        for(int i=0;i<numCourses;i++){
+            if(state[i] !=2 and !dfs(i,adj,state)){
+               return false;
+            }
         }
         return true;
     }
