2248a5d5af443e17ebd4984d666b03b8c0b39dbd
Parth Dhengle <parthdhengle12@gmail.com>
2026-07-15 07:31:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (5.68%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    void back_track(int k, string &ele, string &digits,vector<string>&number, vector<string>&ans,int n){
-        if(k==n){
-            ans.push_back(ele);
+    void combi(string digits, vector<string>&ans, int i,vector<string>hash,string s){
+        int n=digits.size();
+        if(i==n){
+            ans.push_back(s);
             return;
         }
-        int curr=digits[k]-'2';
-        for(int i=0;i<number[curr].size();i++){
-            ele+=number[curr][i];
-            k++;
-            back_track(k,ele,digits,number,ans,n);
-            k--;
-            ele.pop_back();
+        
+        int digit=digits[i]- '0';
+        string curr=hash[digit];
+        
+        for(char c:curr){
+            string ns=s+c;
+            combi(digits,ans,i+1,hash,ns);
         }
     }
     vector<string> letterCombinations(string digits) {
-        vector<string> number = {"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-
         int n=digits.size();
-
+        vector<string>hash={"-1","-1","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         vector<string>ans;
-        string ele="";
-
-        back_track(0,ele,digits,number,ans,n);
+        combi(digits,ans,0,hash,"");
         return ans;
     }
 };
