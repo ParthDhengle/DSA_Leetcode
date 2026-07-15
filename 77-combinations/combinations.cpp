e0a305bdfb6d31a806b72abb221a3b4cdc7928df
Parth Dhengle <parthdhengle12@gmail.com>
2026-07-15 08:16:57 +0530

    Time: 35 ms (91.56%) | Memory: 62.3 MB (96.98%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -6,10 +6,11 @@ void rec(int start, int n,int k,vector<vector<int>>&ans,vector<int>&temp){
         return;
     }
     if(start>n)return;
-    temp.push_back(start);
-    rec(start+1,n,k-1,ans,temp);
-    temp.pop_back();
-    rec(start+1,n,k,ans,temp);
+    for(int i=start;i<=n;i++){
+        temp.push_back(i);
+        rec(i+1,n,k-1,ans,temp);
+        temp.pop_back();
+    }
 
 }
     vector<vector<int>> combine(int n, int k) {
