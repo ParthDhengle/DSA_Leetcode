33402b805263d796abf39250754ee78eb388a325
Parth Dhengle <parthdhengle12@gmail.com>
2026-07-30 23:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (58.37%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int n1=a.size();
-        int n2=b.size();
-        if(n2>n1)return addBinary(b,a);
-        reverse(a.begin(),a.end());
-        reverse(b.begin(),b.end());
-        string ans;
+        int i=a.size()-1;
+        int j=b.size()-1;
+        string ans="";
         int carry=0;
-        for(int i=0;i<n2;i++){
-            int sum=carry+a[i]-'0'+b[i]-'0';
-            if(sum<2){
-                ans+=sum+'0';
-                carry=0;
+        while(i>=0 or j>=0 or carry){
+            if(i>=0){
+                carry+=a[i]-'0';
+                i--;
             }
-            if(sum==2){
-                ans+='0';
-                carry=1;
-            }if(sum==3){
-                ans+='1';
-                carry=1;
+            if(j>=0){
+                carry+=b[j]-'0';
+                j--;
             }
-        }
-        for(int i=n2;i<n1;i++){
-            int sum=a[i]-'0'+carry;
-            if(sum<2){
-                ans+=sum+'0';
-                carry=0;
-            }else if(sum==2){
-                carry=1;
-                ans+='0';
-            }
-        }
-        if(carry){
-            ans+='1';
+            ans+=carry%2 +'0';
+            carry/=2;
         }
         reverse(ans.begin(),ans.end());
         return ans;
