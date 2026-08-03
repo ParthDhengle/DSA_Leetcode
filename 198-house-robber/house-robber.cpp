594b19e3e25360f28328bae1cb12b5b92b3107bb
Parth Dhengle <parthdhengle12@gmail.com>
2026-08-03 23:22:56 +0530

    Time: 2 ms (7.52%) | Memory: 10.7 MB (24.33%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    int rec(vector<int>&nums, int curr, vector<int>&dp){
         int n=nums.size();
-        if(n<2)return nums[0];
-        vector<int>dp(n,0);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            dp[i]=max(dp[i-2]+nums[i],dp[i-1]);
-        }
-        return dp[n-1];
+        if(curr==n-1)return nums[curr];
+        if(curr>=n)return 0;
+        if(dp[curr]!=-1){return dp[curr];}
+        int ans=max(rec(nums,curr+2,dp) + nums[curr], rec(nums,curr+1,dp)) ;
+        dp[curr]=ans;
+        return ans;
+        
+    }
+    int rob(vector<int>& nums) {
+        vector<int>dp(nums.size(),-1);
+        return rec(nums,0,dp);
     }
 };
