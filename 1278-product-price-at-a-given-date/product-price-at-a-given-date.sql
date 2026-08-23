11031c8dc7ca9e7b5077a42c8a6b969d5b16a035
Parth Dhengle <parthdhengle12@gmail.com>
2026-08-23 12:44:33 +0530

    Time: 1608 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select p.product_id,
+COALESCE((
+    select pr.new_price from products pr
+    where pr.product_id = p.product_id and pr.change_date<='2019-08-16'
+    order by pr.change_date desc limit 1
+),10) as price
+from Products as p
+group by p.product_id;
