a12a3e54d76bc00fe5c22be51d02de1c148b4732
Parth Dhengle <parthdhengle12@gmail.com>
2026-09-07 21:28:21 +0530

    Time: 13 ms (70.17%) | Memory: 38.7 MB (98.73%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.py
@@ -1,24 +1,14 @@
 class Solution:
-    def lower_bound(self,x,arr):
-        left,right=0,len(arr)
-        while left<right:
-            mid= left+(right-left)//2
-            if arr[mid]>=x:
-                right=mid
-            else:
-                left=mid+1
-        return left
     def increasingTriplet(self, nums: List[int]) -> bool:
         if len(nums)<3:
             return False
-        lst=[]
+        n1 = float('inf')
+        n2 = float('inf')
         for i in nums:
-            lb=self.lower_bound(i,lst)
-            if lb==len(lst):
-                lst.append(i)
+            if n1>=i:
+                n1=i
+            elif n2>=i:
+                n2=i
             else:
-                lst[lb]=i
-            if len(lst)==3:
                 return True
         return False
-            
